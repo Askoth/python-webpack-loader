c50684b6e1960070c3687004fd82d402bad91415
Marcelo Oliveira <askoth@gmail.com>
2018-03-20 00:01:55 +0100

    Making webpack loader lose track of files to watch

--- examples/hello/hello.py
+++ examples/hello/hello.py
@@ -1,2 +1,8 @@
-def hello_world():
-   print("Hello World")
+import world
+
+def hello():
+   return "Hello " + world.world();
+
+# print(hello())
+
+module.exports = {"hello_world": hello}
--- examples/hello/main.js
+++ examples/hello/main.js
@@ -1,1 +1,3 @@
 import Hello from './hello.py'
+
+document.querySelector('#app').innerHTML = Hello.hello_world();
